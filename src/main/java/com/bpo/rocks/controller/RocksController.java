9dfb9bdbe12a58a132fc6e182a5bdcb92a69aae2
brunopirizoliveira <brunopirizoliveira@gmail.com>
2022-06-13 13:53:18 -0300

    Add Response Entity in return

--- src/main/java/com/bpo/rocks/controller/RocksController.java
+++ src/main/java/com/bpo/rocks/controller/RocksController.java
@@ -2,7 +2,10 @@ package com.bpo.rocks.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.Response;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,8 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class RocksController {
     
     @RequestMapping(value="/band", method=RequestMethod.GET)
-    public String band(HttpServletRequest request, HttpServletResponse response, @RequestHeader("uuid") String uuid)
+    public ResponseEntity<String> band(HttpServletRequest request, HttpServletResponse response, @RequestHeader("uuid") String uuid)
     {
-        return uuid;
+        return new ResponseEntity<String>(uuid, HttpStatus.OK);
     }
 }
